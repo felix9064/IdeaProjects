8c84d63366b30a74a9797b556a21f7911cc790e2
felix9064 <zhang1zhi7hua5@gmail.com>
2016-06-30 08:49:54 +0800

    提交ThinkingJava代码

---
+++ ThinkJava/src/com/felix/thinkjava/chapter08/TestSubClass10.java
@@ -0,0 +1,10 @@
+package com.felix.thinkjava.chapter08;
+
+/**
+ * Created by Felix on 2016/6/4.
+ */
+public class TestSubClass10 extends TestSuperClass10 {
+
+
+
+}
---
+++ ThinkJava/src/com/felix/thinkjava/chapter08/TestSuperClass10.java
@@ -0,0 +1,16 @@
+package com.felix.thinkjava.chapter08;
+
+/**
+ * Created by Felix on 2016/6/4.
+ */
+public class TestSuperClass10 {
+
+    public void print(String str) {
+        System.out.println("输出：" + str);
+    }
+
+    public void doSomething() {
+        print("Hello World!");
+    }
+
+}
