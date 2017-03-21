5349e1a4511dfda4c8246c99417c9d0e58456b0b
felix9064 <zhang1zhi7hua5@gmail.com>
2017-03-21 15:50:51 +0800

    无符号运算

---
+++ CrazyJava/src/com/felix/crazyjava/item0601/UnsignedTest.java
@@ -0,0 +1,24 @@
+package com.felix.crazyjava.item0601;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Java 8 针对整型包装类提供了支持无符号运算的方法
+ * Author: Felix
+ * Date: 2017/3/20
+ * Time: 12:51
+ */
+public class UnsignedTest {
+
+    public static void main(String[] args) {
+        byte b = -3;
+
+        // 将byte类型的-3转换为无符号整数
+        System.out.println("byte类型的-3对应的无符号整数：" + Byte.toUnsignedInt(b));
+
+        int val = Integer.parseUnsignedInt("ab", 16);
+        System.out.println(val);
+
+        System.out.println(Integer.toUnsignedString(-12, 16));
+
+    }
+}
