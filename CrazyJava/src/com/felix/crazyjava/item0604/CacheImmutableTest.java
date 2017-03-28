8ccdc60a31d9a06b98ac41f3d62c09d359e9de0c
felix9064 <zhang1zhi7hua5@gmail.com>
2017-03-28 16:36:55 +0800

    final关键字的用法

---
+++ CrazyJava/src/com/felix/crazyjava/item0604/CacheImmutableTest.java
@@ -0,0 +1,68 @@
+package com.felix.crazyjava.item0604;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 将经常使用的不可变类的实例缓存起来，提高系统性能
+ * Author: Felix
+ * Date: 2017/3/28
+ * Time: 14:21
+ */
+public class CacheImmutableTest {
+
+    public static void main(String[] args) {
+        CacheImmutable c1 = CacheImmutable.valueOf("hello");
+        CacheImmutable c2 = CacheImmutable.valueOf("hello");
+        System.out.println(c1 == c2);
+    }
+
+}
+
+class CacheImmutable {
+    private static int MAX_SIZE = 10;
+    private static CacheImmutable[] cache = new CacheImmutable[MAX_SIZE];
+
+    private static int pos = 0;
+    private final String name;
+
+    private CacheImmutable(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static CacheImmutable valueOf(String name) {
+
+        for (int i = 0; i < MAX_SIZE; i++) {
+            if(cache[i] != null && cache[i].getName().equals(name)) {
+                return cache[i];
+            }
+        }
+        if(pos == MAX_SIZE) {
+            cache[0] = new CacheImmutable(name);
+            pos = 1;
+        } else {
+            cache[pos++] = new CacheImmutable(name);
+        }
+
+        return cache[pos-1];
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj != null && obj.getClass() == CacheImmutable.class) {
+            CacheImmutable ci = (CacheImmutable) obj;
+            return name.equals(ci.getName());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
---
+++ CrazyJava/src/com/felix/crazyjava/item0604/Person.java
@@ -0,0 +1,72 @@
+package com.felix.crazyjava.item0604;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 带有引用类型成员变量的不可变类
+ * Author: Felix
+ * Date: 2017/3/28
+ * Time: 13:54
+ */
+public class Person {
+    private final Name name;
+
+    /*
+    public Person(Name name) {
+        this.name = name;
+    }
+    */
+
+    /*
+    public Name getName() {
+        return name;
+    }
+    */
+
+    public Person(Name name) {
+        this.name = new Name(name.getFirstName(), name.getLastName());
+    }
+
+    public Name getName() {
+        return new Name(name.getFirstName(), name.getLastName());
+    }
+
+    public static void main(String[] args) {
+        Name n = new Name("三", "张");
+        Person p = new Person(n);
+
+        System.out.println(p.getName().getFirstName());
+        n.setFirstName("四");
+        System.out.println(p.getName().getFirstName());
+
+    }
+}
+
+class Name{
+
+    private String firstName;
+    private String lastName;
+
+    public Name() {
+    }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
