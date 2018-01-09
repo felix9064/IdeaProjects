95c8ccebdd42607bb8b6c2f197cc1ad6dee78221
felix <zhang1zhi7hua5@gmail.com>
2018-01-09 10:18:33 +0800

    Java子类依次调用父类构造器

---
+++ CrazyJava/src/com/felix/crazyjava/item0506/Wolf.java
@@ -0,0 +1,39 @@
+package com.felix.crazyjava.item0506;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 子类依次调用父类构造器，从继承树的最顶端依次往下执行
+ * Author: Felix
+ * Date: 2018/1/5
+ * Time: 16:34
+ */
+
+class Creature {
+
+    public Creature() {
+        System.out.println("Creature无参数的构造器");
+    }
+}
+
+class Animal extends Creature {
+
+    public Animal(String name) {
+        System.out.println("Animal带一个参数的构造器，该动物的name为：" + name);
+    }
+
+    public Animal(String name, int age) {
+        this(name);
+        System.out.println("Animal带两个参数的构造器，其age为：" + age);
+    }
+}
+
+public class Wolf extends Animal {
+    public Wolf() {
+        super("灰太狼", 3);
+        System.out.println("Wolf无参数的构造器");
+    }
+
+    public static void main(String[] args) {
+        new Wolf();
+    }
+}
