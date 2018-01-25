6397f8ab8f48abd4f83f7d98b369fc29dacc1a70
felix <zhang1zhi7hua5@gmail.com>
2018-01-25 10:20:47 +0800

    基本数据包装类型和字符串互转

--- CrazyJava/src/com/felix/crazyjava/item0601/Primitive2String.java
+++ CrazyJava/src/com/felix/crazyjava/item0601/Primitive2String.java
@@ -17,23 +17,23 @@ public class Primitive2String {
         int it2 = new Integer(intStr);
         System.out.println(it1 == it2);
 
-        /**
+        /*
          * 下面两个输出表达式的比较结果之所以不同，是与java的Integer类的设计有关
          * 系统把一个 -128~127之间的整数自动装箱成Integer实例，并放入了一个名为cache的数组中缓存起来
          * 如果以后把一个-128~127之间的整数自动装箱成Integer实例时，实际上是直接指向对应的数组元素
          * 因此-128~127之间的同一个整数自动装箱成Integer实例时，永远都是引用cache数组的同一个数组元素，所以他们是相等的
          * 但是每次把一个不在-128~127范围内的整数自动装箱成Integer实例时，系统总是重新创建一个Integer实例，所以他们不相等
          */
-        Integer int1 = 127;
-        Integer int2 = 127;
+        Integer int1 = 127, int2 = 127;
         System.out.println(int1 == int2);
 
-        Integer a = 128;
-        Integer b = 128;
-        System.out.println(a == b);
-        System.out.println(Integer.compare(a, b));
+        Integer a = 128, b = 128;
+        System.out.println("Integer类型 a==b 结果：" + (a == b));
+        System.out.println("Integer类型 a.equals(b) 结果：" + a.equals(b));
 
-        System.out.println(Integer.parseUnsignedInt("7FF" , 16));
-        System.out.println(Integer.parseUnsignedInt("FFFFFFF6", 16));
+        int x = 129, y = 129;
+        System.out.println("int类型 x==y 结果：" + (x == y));
+
+        System.out.println(Integer.compare(a, b));
     }
 }
